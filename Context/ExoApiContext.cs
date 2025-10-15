5e17f7fa1262a5fe09f6b444e4e6f000e421daac
Anderson Alexandre <Andersonalexandre1228@gmail.com>
2025-10-15 20:04:12 -0300

    primeiro commit

---
+++ Context/ExoApiContext.cs
@@ -0,0 +1,23 @@
+﻿using ExoApi.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExoApi.Contexts
+{
+    public class ExoApiContext : DbContext
+    {
+        public ExoApiContext()
+        { 
+        }
+        public ExoApiContext(DbContextOptions<ExoApiContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-17TNER5\\SQLEXPRESS;Initial Catalog=ExoApi;Integrated Security=True;TrustServerCertificate=True;");
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+    }
+}
